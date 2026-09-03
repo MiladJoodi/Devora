ee30ea8987373e15fb62a6e1da7f934ba904276b
Joodi <winbeta.com@gmail.com>
2026-09-04 03:21:54 +0330

    Update post.ts

--- validations/post.ts
+++ validations/post.ts
@@ -2,10 +2,16 @@ import { z } from "zod";
 
 export const createPostSchema = z.object({
   title: z.string().min(3).max(200),
-
   content: z.string().min(10),
-
   categoryId: z.string().uuid(),
 });
 
 export type CreatePostInput = z.infer<typeof createPostSchema>;
+
+export const updatePostSchema = z.object({
+  title: z.string().min(3).max(200),
+  content: z.string().min(10),
+  categoryId: z.string().uuid(),
+});
+
+export type UpdatePostInput = z.infer<typeof updatePostSchema>;
